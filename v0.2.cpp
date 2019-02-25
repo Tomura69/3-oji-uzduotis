9ac365975bd1b03b7b6f933beb7041a0d117be03
Tomas <tomas.klevas@mif.vu.lt>
2019-02-25 21:46:07 +0200

    v0.3

--- v0.2.cpp
+++ v0.2.cpp
@@ -1,102 +1,4 @@
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <time.h>
-#include <ctype.h>
-#include <vector>
-#include <fstream>
-#include <stdlib.h>
-#include <array>
-
-using std::cout;
-using std::cin;
-using std::endl;
-
-struct duom
-{
-    std::string var, pav;
-    double galutinis, mediana, galmed;
-};
-
-bool Patikra (std::string a, std::string eroras){
-    for (int i = 0; i < a.size(); i++){
-                if (isalpha(a[i]) == 0){
-                    cout << eroras << endl;
-                    return 1;
-                    break;
-                }
-            }
-}
-
-void Skaitymas (int & m, std::vector<duom>& Duomenys, const std::string eroras){
-    std::ifstream fd("kursiokai.txt");
-    int laik;
-    
-    std::string pvz;
-    //bool break1 = 0;
-    std::getline(fd, pvz);
-    while (!fd.eof()){
-        std::vector<int> Medv;
-        int sum = 0, kas = 0;
-        Duomenys.push_back(duom());
-        fd >> Duomenys[m].var;
-        if (Patikra (Duomenys[m].var, eroras)) break;
-        fd >> Duomenys[m].pav;
-        if (Patikra (Duomenys[m].pav, eroras)) break;
-        std::getline(fd, pvz);
-        for (int i = 0; i < pvz.size(); i++){
-            if (pvz[i] != ' '){
-                laik = pvz[i] - '0';
-                if (pvz[i] == '1' && pvz[i+1] == '0'){
-                    laik = 10;
-                    Medv.push_back(laik);
-                    kas++;
-                    i++;
-                }
-                else if (laik < 1 || laik > 10){
-                }
-                else {
-                    Medv.push_back(laik);
-                    kas++;
-                }
-            }
-        }
-        pvz = '0';
-        if (Medv.empty()){
-            cout << eroras << endl;
-            break;
-        }
-        int* Med = new int(kas);
-        std::copy(Medv.begin(), Medv.end(), Med);
-        
-        for (int l = 0; l < kas-1; l++){
-                for (int k = l+1; k < kas; k++){
-                    if (Med[l] > Med[k])
-                        std::swap(Med[l], Med[k]);
-                }
-        }
-        int egz = Med[kas-1];
-        kas--;
-        for (int i = 0; i < kas; i++){
-            sum = sum + Med[i];
-        }
-        bool lyginis = (kas%2 == 0);
-        if (lyginis){
-            double tarp2 = (Med[kas/2-1] + Med[kas/2]) / 2.0;
-            Duomenys[m].mediana = tarp2;
-        }
-        else {
-            Duomenys[m].mediana = Med[(kas/2)];
-        }
-        double tarp = 0.4*((double)sum / kas)+0.6*egz;
-        Duomenys[m].galutinis = tarp;
-        Duomenys[m].galmed = 0.4*Duomenys[m].mediana+0.6*egz;
-        m++;
-        delete [] Med;
-        Medv.clear();
-    } 
-    fd.close();
-}
+#include "Headers.h"
 
 int main (int argc, char *argv[]){
     const std::string eroras = "Blogai ivesti duomenys";
@@ -106,16 +8,21 @@ int main (int argc, char *argv[]){
     std::vector<duom> Duomenys;
     
     srand(time(NULL));
-    Skaitymas (m, Duomenys, eroras);
+    Skaitymas (m, Duomenys);
     while (true){
         n = 0;
         sum = 0;
         cout << "Ivesk varda ir pavarde" << endl;
         Duomenys.push_back(duom());
-        cin >> Duomenys[m].var;
-        if (Patikra (Duomenys[m].var, eroras)) break;
-        cin >> Duomenys[m].pav;
-        if (Patikra (Duomenys[m].pav, eroras)) break;
+        try {
+            cin >> Duomenys[m].var;
+            Patikra (Duomenys[m].var);
+            cin >> Duomenys[m].pav;
+            Patikra (Duomenys[m].pav);
+        } catch (const char* msg){
+            cout << msg << endl;
+            break;
+        }
         char d;
         cout << "Jei norite, kad pazymiai butu generuojami atsitiktinai veskite 1, kitu atveju veskite 0" << endl;
         cin >> d;
